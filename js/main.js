6c7c93446b362309f73150ee7f8c9ba41b6bd86d
Benjamin-sa <Benkee.sauter@gmail.com>
2024-02-06 22:20:05 +0100

    responsive update

--- js/main.js
+++ js/main.js
@@ -12,16 +12,21 @@ window.addEventListener('load', function() {
     }, 500); // Vertraging van halve seconde
 });
 
-$(window).on('scroll', function() {
-    var scrollTop = $(this).scrollTop();
-    var rotationAngle = -scrollTop / 10; // Pas de deler aan om de rotatiesnelheid te wijzigen
+var scrollTop, rotationAngle;
+
+function animate() {
+    scrollTop = $(window).scrollTop();
+    rotationAngle = -scrollTop / 10; // Pas de deler aan om de rotatiesnelheid te wijzigen
 
     $('.carousel-item, .carousel-foto-center, #section2').css({
         'transform': 'rotateX(' + rotationAngle + 'deg)',
         'transform-origin': 'bottom center',
-        'position': 'static'
     });
-});
+
+    requestAnimationFrame(animate);
+}
+
+animate();
 
 // center image laten bewegen
 window.addEventListener('scroll', function() {
@@ -29,7 +34,7 @@ window.addEventListener('scroll', function() {
     var scroll = window.pageYOffset;
 
     // Bepaal de schaal van de afbeelding op basis van de scroll positie
-    var scale = Math.min(1 + scroll / 1500, 1.5);
+    var scale = Math.min(1 + scroll / 2500, 1.5);
 
     // Selecteer de afbeelding
     var img = document.querySelector('.carousel-centered-image img');
