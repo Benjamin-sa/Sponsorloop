85330ba520ec0c9bb301b4db2eef0712bef92273
Benjamin-sa <Benkee.sauter@gmail.com>
2024-03-11 20:48:45 +0100

    Add dependencies and email sending functionality

--- js/donatie.js
+++ js/donatie.js
@@ -1,45 +1,45 @@
-firebase.auth().onAuthStateChanged(user => {
-    if (!user) {
-        window.location.href = "index.html";
-    }
+firebase.auth().onAuthStateChanged((user) => {
+  if (!user) {
+    window.location.href = "index.html";
+  }
 });
 /*----------------------------------------------------*/
 /*   verbiedt dubbele bedragen
 /*----------------------------------------------------*/
-const customAmountInput = document.querySelector('.form-control');
-const radioButtons = document.querySelectorAll('.form-check-input');
+const customAmountInput = document.querySelector(".form-control");
+const radioButtons = document.querySelectorAll(".form-check-input");
 
-customAmountInput.addEventListener('input', () => {
-    radioButtons.forEach(radioButton => {
-        radioButton.checked = false;
-    });
+customAmountInput.addEventListener("input", () => {
+  radioButtons.forEach((radioButton) => {
+    radioButton.checked = false;
+  });
 });
-radioButtons.forEach(radioButton => {
-    radioButton.addEventListener('change', () => {
-        customAmountInput.value = '';
-    });
+radioButtons.forEach((radioButton) => {
+  radioButton.addEventListener("change", () => {
+    customAmountInput.value = "";
+  });
 });
 /*----------------------------------------------------*/
 /*   pop up menu
 /*----------------------------------------------------*/
 
 const submitButton = document.querySelector('button[type="submit"]');
-submitButton.addEventListener('click', event => {
-    event.preventDefault();
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
 
-    const selectedNames = getSelectedNames();
-    const amount = getSelectedAmount();
+  const selectedNames = getSelectedNames();
+  const amount = getSelectedAmount();
 
-    if (selectedNames.length > 0 && amount) {
-        const popup = document.getElementById('confirmation');
-        popup.style.display = 'block';
-    } else {
-        alert('Selecteer a.u.b. een naam en een bedrag voordat u verdergaat.');
-    }
+  if (selectedNames.length > 0 && amount) {
+    const popup = document.getElementById("confirmation");
+    popup.style.display = "block";
+  } else {
+    alert("Selecteer a.u.b. een naam en een bedrag voordat u verdergaat.");
+  }
 });
 
-document.getElementById('close').addEventListener('click', function() {
-    document.getElementById('confirmation').style.display = 'none';
+document.getElementById("close").addEventListener("click", function () {
+  document.getElementById("confirmation").style.display = "none";
 });
 
 /*----------------------------------------------------*/
@@ -50,72 +50,106 @@ let parsedData = null;
 
 // Functie om de CSV-gegevens op te halen en te verwerken
 async function parseCsvData() {
-    const response = await fetch('js/ledenlijst.csv');
-    const data = await response.text();
-    parsedData = Papa.parse(data, { header: true, dynamicTyping: true, delimiter: ",", skipEmptyLines: true }).data;
+  const response = await fetch("js/ledenlijst.csv");
+  const data = await response.text();
+  parsedData = Papa.parse(data, {
+    header: true,
+    dynamicTyping: true,
+    delimiter: ",",
+    skipEmptyLines: true,
+  }).data;
 }
 
 // Functie om de overeenkomende 'Takken' waarde te vinden voor een gegeven naam
 function findMatchingTak(name) {
-    let matchingRow = parsedData.find(row => row['Naam'] === name);
-    return matchingRow ? matchingRow['Takken'] : alert('Geen tak gevonden voor ' + name);
+  let matchingRow = parsedData.find((row) => row["Naam"] === name);
+  return matchingRow
+    ? matchingRow["Takken"]
+    : alert("Geen tak gevonden voor " + name);
 }
 
 // Functie om de geselecteerde namen op te halen
 function getSelectedNames() {
-    const selectedNamesDiv = document.getElementById('geselecteerdeNamen');
-    return Array.from(selectedNamesDiv.children).map(child => child.textContent.slice(0, -1));
+  const selectedNamesDiv = document.getElementById("geselecteerdeNamen");
+  return Array.from(selectedNamesDiv.children).map((child) =>
+    child.textContent.slice(0, -1)
+  );
 }
 
 // Functie om het geselecteerde bedrag op te halen
 function getSelectedAmount() {
-    const radios = document.getElementsByName('flexRadioDefault');
-    for (let i = 0, length = radios.length; i < length; i++) {
-        if (radios[i].checked) {
-            return radios[i].value;
-        }
+  const radios = document.getElementsByName("flexRadioDefault");
+  for (let i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      return radios[i].value;
     }
-    return document.querySelector('input[aria-label="hoeveelheid"]').value;
+  }
+  return document.querySelector('input[aria-label="hoeveelheid"]').value;
 }
 
-document.getElementById('Goed').addEventListener('click', async () => {
-    console.log(firebase.auth().currentUser);
-    if (!parsedData) {
-        await parseCsvData();
-    }
-    const userID = firebase.auth().currentUser.uid;
-    console.log(userID);
-    const username = firebase.auth().currentUser.displayName;
-    const email = firebase.auth().currentUser.email;
-    const amount = getSelectedAmount();
-    const selectedNames = getSelectedNames();
+document.getElementById("Goed").addEventListener("click", async () => {
+  console.log(firebase.auth().currentUser);
+  if (!parsedData) {
+    await parseCsvData();
+  }
+  const userID = firebase.auth().currentUser.uid;
+  console.log(userID);
+  const username = firebase.auth().currentUser.displayName;
+  const email = firebase.auth().currentUser.email;
+  const amount = getSelectedAmount();
+  const selectedNames = getSelectedNames();
 
-    let donateurRef = db.collection('Donateurs').doc(userID);
+  let donateurRef = db.collection("Donateurs").doc(userID);
 
-    donateurRef.set({
-        Naam: username,
-        Email: email
-    }, { merge: true });
+  donateurRef.set(
+    {
+      Naam: username,
+      Email: email,
+    },
+    { merge: true }
+  );
 
-    for (let name of selectedNames) {
-        let tak = findMatchingTak(name);
-        if (tak) {
-            let donatieRef = donateurRef.collection('Donaties').doc();
+  for (let name of selectedNames) {
+    let tak = findMatchingTak(name);
+    if (tak) {
+      let donatieRef = donateurRef.collection("Donaties").doc();
 
-            donatieRef.set({
-                LidID: name,
-                Bedrag: amount
-            }).then(() => {
-                alert("De donatie is succesvol verwerkt");
-                window.location.href = "index.html";
-            }).catch((error) => {
-                console.error("Error writing document: ", error);
-                if (error.code === 'resource-exhausted') {
-                    alert('Limiet is vandaag bereikt, probeer het morgen opnieuw');
-                } else {
-                    alert('Er is een fout opgetreden bij het schrijven van de gegevens');
-                }
+      donatieRef
+        .set({
+          LidID: name,
+          Bedrag: amount,
+        })
+        .then(() => {
+          alert("De donatie is succesvol verwerkt");
+
+          // Stuur een POST-verzoek naar de server om de bevestigingsmail te verzenden
+          fetch("/send-email", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              to: email,
+              subject: "Donatie bevestiging",
+              text: `Bedankt voor je donatie van ${amount} aan ${name}!`,
+            }),
+          })
+            .then((response) => response.text())
+            .then((data) => console.log(data))
+            .catch((error) => {
+              console.error("Error:", error);
             });
-        }
+        })
+        .catch((error) => {
+          console.error("Error writing document: ", error);
+          if (error.code === "resource-exhausted") {
+            alert("Limiet is vandaag bereikt, probeer het morgen opnieuw");
+          } else {
+            alert(
+              "Er is een fout opgetreden bij het schrijven van de gegevens"
+            );
+          }
+        });
     }
+  }
 });
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+
+
 
 app.use(express.static(path.join(__dirname, '.')));
+app.use(bodyParser.json());
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -12,3 +20,30 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY || '', url: 'https://api.eu.mailgun.net' });
+
+app.post('/send-email', (req, res) => {
+    const { to, subject, text } = req.body;
+
+    const data = {
+        from: 'Excited User <mailgun@lodlavki-sponsorloop.me>',
+        to: to,
+        subject: subject,
+        text: text
+    };
+
+    mg.messages.create('lodlavki-sponsorloop.me', data)
+        .then(msg => {
+            console.log(msg);
+            res.status(200).send('Email sent');
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Error sending email');
+        });
+});
+
+module.exports = mg;
